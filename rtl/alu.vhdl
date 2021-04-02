d84bf0f069a3b43c6931ea56e378800d6d1cd4c1
root <root@LAPTOP-C4NN74M6.localdomain>
2021-04-02 00:20:36 -0300

    alu_pkg created

--- rtl/alu.vhdl
+++ rtl/alu.vhdl
@@ -1,31 +1,94 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+library work;
+use work.alu_pkg.all;
 
 entity alu is
-    port (
-        opd0, opd1: in std_logic_vector(31 downto 0);
-        op: in std_logic_vector(3 downto 0);
-        rst: out std_logic_vector(31 downto 0);
-        zero: out std_logic        
+
+    port(
+	    opd0, opd1: in  std_logic_vector(31 downto 0);
+	    op: in alu_op;
+	    rslt: out std_logic_vector(31 downto 0);
+        zero: out std_logic
     );
+
 end entity alu;
 
-architecture alu_bhv of alu is
-    
-    signal rst_i: std_logic_vector(31 downto 0);
+architecture alu_arch of alu is
+
+    signal rslt_i: std_logic_vector(31 downto 0) := x"0000_0000";
 
 begin
+
+    process(op, opd0, opd1)
+
+	begin
+
+		case op is
+			
+            when ALU_AND => rslt_i <= opd0 and opd1;
+
+			when ALU_OR => rslt_i <= opd0 or opd1;
+			
+            when ALU_XOR => rslt_i <= opd0 xor opd1;
+			
+            when ALU_SLT =>
+				
+                if signed(opd0) < signed(opd1) then
+				
+                    rslt_i <= (0 => '1', others => '0');
+				
+                else
+				
+                    rslt_i <= (others => '0');
+				
+                end if;
+
+			when ALU_SLTU =>
+
+				if unsigned(opd0) < unsigned(opd1) then
+			
+                    rslt_i <= (0 => '1', others => '0');
+			
+                else
+			
+                    rslt_i <= (others => '0');
+			
+                end if;
+			
+            when ALU_ADD => rslt_i <= std_logic_vector(unsigned(opd0) + unsigned(opd1));
+			
+            when ALU_SUB => rslt_i <= std_logic_vector(unsigned(opd0) - unsigned(opd1));
+			
+            when ALU_SRL => rslt_i <= std_logic_vector(shift_right(unsigned(opd0), to_integer(unsigned(opd1(4 downto 0)))));
+			
+            when ALU_SLL => rslt_i <= std_logic_vector(shift_left(unsigned(opd0), to_integer(unsigned(opd1(4 downto 0)))));
+			
+            when ALU_SRA => rslt_i <= std_logic_vector(shift_right(signed(opd0), to_integer(unsigned(opd1(4 downto 0)))));
+			
+            when others => rslt_i <= (others => '0');
+		
+        end case;
+
+    end process;
+
+    process(rslt_i)
     
-    with op select rst_i <= 
-        std_logic_vector(unsigned(opd0) + unsigned(opd1)) when "0000",
-        std_logic_vector(unsigned(opd0) - unsigned(opd1)) when "0001",
-        opd0 and opd1 when "0010",
-        opd0 or opd1 when "0011",
-        opd0 xor opd1 when "0100",
-        x"0000_0000" when others;
+    begin
 
-    rst <= rst_i;
-    zero <= '1' when (rst_i = x"0000_0000") else '0'; 
+        rslt <= rslt_i;
 
-end architecture alu_bhv;
+        if rslt_i = x"0000_0000" then
+            
+            zero <= '1';
+
+        else
+
+            zero <= '0';
+
+        end if; 
+        
+    end process;
+
+end architecture alu_arch;
--- tbs/alu_tb.vhdl
+++ tbs/alu_tb.vhdl
@@ -1,18 +1,20 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+library work;
+use work.alu_pkg.all;
 
 entity alu_tb is 
 end alu_tb;
 
 architecture alu_tb_arch of alu_tb is
 
-    signal opd0, opd1, rst: std_logic_vector(31 downto 0);
+    signal opd0, opd1, rslt: std_logic_vector(31 downto 0);
     signal op : std_logic_vector(3 downto 0);
     signal zero: std_logic;
 
 begin
     
-    uut: entity work.alu port map (opd0, opd1, op, rst, zero);
+    uut: alu port map (opd0, opd1, op, rslt, zero);
 
     process
 
@@ -24,45 +26,45 @@ begin
 
             opd0 <= x"0001_2CD7";
             opd1 <= x"0000_04EC";
-            op <= b"0000";
+            op <= ALU_ADD;
             wait for period;
-            assert (rst = x"0001_31C3" and zero = '0') 
-            report "test failure: it should exec sum op" severity failure;
+            assert (rslt = x"0001_31C3" and zero = '0') 
+            report "test failure: it should exec add op" severity failure;
 
             -- Test case 2
 
             opd0 <= x"0A05_1AB1";
             opd1 <= x"010B_A1D0";
-            op <= b"0001";
+            op <= ALU_SUB;
             wait for period;
-            assert (rst = x"08F9_78E1" and zero = '0') 
+            assert (rslt = x"08F9_78E1" and zero = '0') 
             report "test failure: it should exec sub op" severity failure;
 
             -- Test case 3
 
             opd0 <= x"AF15_AAC1";
             opd1 <= x"F1FC_B1C1";
-            op <= b"0010";
+            op <= ALU_AND;
             wait for period;
-            assert (rst = x"A114_A0C1" and zero = '0') 
+            assert (rslt = x"A114_A0C1" and zero = '0') 
             report "test failure: it should exec and op" severity failure;
 
             -- Test case 4
 
             opd0 <= x"A765_15C1";
             opd1 <= x"F10C_C121";
-            op <= b"0011";
+            op <= ALU_OR;
             wait for period;
-            assert (rst = x"F76D_D5E1" and zero = '0') 
+            assert (rslt = x"F76D_D5E1" and zero = '0') 
             report "test failure: it should exec or op" severity failure;
 
             -- Test case 5
 
             opd0 <= x"B165_9001";
             opd1 <= x"F75C_1101";
-            op <= b"0100";
+            op <= ALU_XOR;
             wait for period;
-            assert (rst = x"4639_8100" and zero = '0') 
+            assert (rslt = x"4639_8100" and zero = '0') 
             report "test failure: it should exec xor op" severity failure;
 
             wait;
