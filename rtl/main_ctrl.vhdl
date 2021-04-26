dbbafdf7eec76c6fa67959e92d483b78da0a53eb
root <root@LAPTOP-C4NN74M6.localdomain>
2021-04-26 01:59:09 -0300

    main ctrl updates

--- rtl/main_ctrl.vhdl
+++ rtl/main_ctrl.vhdl
@@ -7,9 +7,15 @@ entity main_ctrl is
     
     port (
         opcode: in std_logic_vector(6 downto 0);
-        rf_write_en, rf_write_src: out std_logic;
+        no_op: in std_logic;
+        rf_write_src: out std_logic_vector(1 downto 0);
+        rf_write_en: out std_logic;
+        ig_imm_type: out std_logic_vector(2 downto 0);
+        alu_src0, alu_src1, alu_src0_pass: out std_logic;
+        alu_la_op, alu_imm_op: out std_logic;
         lsu_mode, lsu_en: out std_logic;
-        branch, jal, jalr: out std_logic
+        br_detector_en: out std_logic;
+        if_jmp, if_target_shift: out std_logic
     );
 
 end entity main_ctrl;
@@ -18,32 +24,194 @@ architecture main_ctrl_arch of main_ctrl is
     
 begin
 
-    with opcode select rf_write_en <=
-        '1' when LOGIC_ARITH_OPCODE | LOGIC_ARITH_IMM_OPCODE | JALR_OPCODE | LOAD_OPCODE | JAL_OPCODE, 
-        '0' when others;
-
-    with opcode select rf_write_src <= 
-        '1' when LOAD_OPCODE,
-        '0' when others;
-
-    with opcode select lsu_mode <= 
-        '1' when STORE_OPCODE,
-        '0' when others;
+    process(opcode, no_op)
     
-    with opcode select lsu_en <= 
-        '1' when LOAD_OPCODE | STORE_OPCODE,
-        '0' when others;
+    begin
+        
+        if no_op = '1' then
+            
+            rf_write_src <= "--";
+            rf_write_en <= '0';
+            ig_imm_type <= "---";
+            alu_src0 <= '0';
+            alu_src1 <= '0';
+            alu_src0_pass <= '-';
+            alu_la_op <= '0';
+            alu_imm_op <= '0';
+            lsu_mode <= '-';
+            lsu_en <= '0';
+            br_detector_en <= '0';
+            if_jmp <= '0';
+            if_target_shift <= '0';
 
-    with opcode select branch <= 
-        '1' when BRANCH_OPCODE,
-        '0' when others;
+        else
 
-    with opcode select jal <= 
-        '1' when JAL_OPCODE,
-        '0' when others;
+            case opcode is
+                
+                when LOGIC_ARITH_OPCODE =>
+                    
+                    rf_write_src <= b"00";
+                    rf_write_en <= '1';
+                    ig_imm_type <= "---";
+                    alu_src0 <= '0';
+                    alu_src1 <= '0';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '1';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '0';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
+            
+                when LOGIC_ARITH_IMM_OPCODE =>
+                    
+                    rf_write_src <= b"00";
+                    rf_write_en <= '1';
+                    ig_imm_type <= IMM_I_TYPE;
+                    alu_src0 <= '0';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '1';
+                    alu_imm_op <= '1';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '0';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
 
-    with opcode select jalr <= 
-        '1' when JALR_OPCODE,
-        '0' when others;
+                when JALR_OPCODE =>
+                    
+                    rf_write_src <= b"10";
+                    rf_write_en <= '1';
+                    ig_imm_type <= IMM_I_TYPE;
+                    alu_src0 <= '1';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '0';
+                    if_jmp <= '1';
+                    if_target_shift <= '1';
+
+                when LOAD_OPCODE =>
+                    
+                    rf_write_src <= b"01";
+                    rf_write_en <= '1';
+                    ig_imm_type <= IMM_I_TYPE;
+                    alu_src0 <= '0';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '0';
+                    lsu_en <= '1';
+                    br_detector_en <= '0';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
+
+                when STORE_OPCODE =>
+                    
+                    rf_write_src <= "--";
+                    rf_write_en <= '0';
+                    ig_imm_type <= IMM_S_TYPE;
+                    alu_src0 <= '0';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '1';
+                    lsu_en <= '1';
+                    br_detector_en <= '0';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
+
+                when BRANCH_OPCODE =>
+                    
+                    rf_write_src <= "--";
+                    rf_write_en <= '0';
+                    ig_imm_type <= IMM_B_TYPE;
+                    alu_src0 <= '1';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '1';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
+
+                when LOAD_UPPER_IMM_OPCODE =>
+                    
+                    rf_write_src <= b"00";
+                    rf_write_en <= '1';
+                    ig_imm_type <= IMM_U_TYPE;
+                    alu_src0 <= '-';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '0';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '0';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
+
+                when ADD_UPPER_IMM_PC_OPCODE =>
+                    
+                    rf_write_src <= b"00";
+                    rf_write_en <= '1';
+                    ig_imm_type <= IMM_U_TYPE;
+                    alu_src0 <= '1';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '0';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
+
+                when JAL_OPCODE =>
+                    
+                    rf_write_src <= b"10";
+                    rf_write_en <= '1';
+                    ig_imm_type <= IMM_J_TYPE;
+                    alu_src0 <= '1';
+                    alu_src1 <= '1';
+                    alu_src0_pass <= '1';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '0';
+                    if_jmp <= '1';
+                    if_target_shift <= '0';
+
+                when others =>
+                    
+                    rf_write_src <= "--";
+                    rf_write_en <= '0';
+                    ig_imm_type <= "---";
+                    alu_src0 <= '0';
+                    alu_src1 <= '0';
+                    alu_src0_pass <= '-';
+                    alu_la_op <= '0';
+                    alu_imm_op <= '0';
+                    lsu_mode <= '-';
+                    lsu_en <= '0';
+                    br_detector_en <= '0';
+                    if_jmp <= '0';
+                    if_target_shift <= '0';
+            
+            end case;
+
+        end if;
+
+    end process;
     
 end architecture main_ctrl_arch;
--- tbs/main_ctrl_tb.vhdl
+++ tbs/main_ctrl_tb.vhdl
@@ -9,26 +9,39 @@ end main_ctrl_tb;
 architecture main_ctrl_tb_arch of main_ctrl_tb is
 
     signal opcode: std_logic_vector(6 downto 0);
-    signal rf_write_en, rf_write_src: std_logic;
+    signal no_op: std_logic;
+    signal rf_write_src: std_logic_vector(1 downto 0);
+    signal rf_write_en: std_logic;
+    signal ig_imm_type: std_logic_vector(2 downto 0);
+    signal alu_src0, alu_src1, alu_src0_pass: std_logic;
+    signal alu_la_op, alu_imm_op: std_logic;
     signal lsu_mode, lsu_en: std_logic;
-    signal branch, jal, jalr: std_logic;
+    signal br_detector_en: std_logic;
+    signal if_jmp, if_target_shift: std_logic;
 
 begin
     
     uut: main_ctrl port map (
-        opcode, 
-        rf_write_en, 
-        rf_write_src, 
+        opcode,
+        no_op,
+        rf_write_src,
+        rf_write_en,
+        ig_imm_type,
+        alu_src0, 
+        alu_src1, 
+        alu_src0_pass,
+        alu_la_op, 
+        alu_imm_op,
         lsu_mode, 
-        lsu_en, 
-        branch, 
-        jal, 
-        jalr
+        lsu_en,
+        br_detector_en,
+        if_jmp, 
+        if_target_shift
     );
 
     process
     
-        constant period: time := 50 ns;
+        constant period: time := 10 ns;
 
     begin
         
@@ -36,85 +49,181 @@ begin
             
         wait for period;
 
-        assert (rf_write_en ='1');
-        assert (rf_write_src = '0');
-        assert (lsu_mode = '0');
-        assert (lsu_en = '0');
-        assert (branch = '0');
-        assert (jal = '0');
-        assert (jalr = '0');
-    
-        opcode <= LOGIC_ARITH_IMM_OPCODE;
+        assert rf_write_src = b"00";
+        assert rf_write_en = '1';
+        assert ig_imm_type = "---";
+        assert alu_src0 = '0';
+        assert alu_src1 = '0';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '1';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '0';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
 
-        wait for period;
+        opcode <= LOGIC_ARITH_IMM_OPCODE;
             
-        assert (rf_write_en ='1');
-        assert (rf_write_src = '0');
-        assert (lsu_mode = '0');
-        assert (lsu_en = '0');
-        assert (branch = '0');
-        assert (jal = '0');
-        assert (jalr = '0');
+        wait for period;
+
+        assert rf_write_src = b"00";
+        assert rf_write_en = '1';
+        assert ig_imm_type = IMM_I_TYPE;
+        assert alu_src0 = '0';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '1';
+        assert alu_imm_op = '1';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '0';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
 
         opcode <= JALR_OPCODE;
-        
-        wait for period;
             
-        assert (rf_write_en = '1');
-        assert (rf_write_src = '0');
-        assert (lsu_mode = '0');
-        assert (lsu_en = '0');
-        assert (branch = '0');
-        assert (jal = '0');
-        assert (jalr = '1');
+        wait for period;
+
+        assert rf_write_src = b"10";
+        assert rf_write_en = '1';
+        assert ig_imm_type = IMM_I_TYPE;
+        assert alu_src0 = '1';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '0';
+        assert if_jmp = '1';
+        assert if_target_shift = '1';
 
         opcode <= LOAD_OPCODE;
-
-        wait for period;
             
-        assert (rf_write_en ='1');
-        assert (rf_write_src = '1');
-        assert (lsu_mode = '0');
-        assert (lsu_en = '1');
-        assert (branch = '0');
-        assert (jal = '0');
-        assert (jalr = '0');
+        wait for period;
+
+        assert rf_write_src = b"01";
+        assert rf_write_en = '1';
+        assert ig_imm_type = IMM_I_TYPE;
+        assert alu_src0 = '0';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '0';
+        assert lsu_en = '1';
+        assert br_detector_en = '0';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
 
         opcode <= STORE_OPCODE;
-
+            
         wait for period;
 
-        assert (rf_write_en = '0');
-        assert (rf_write_src = '0');
-        assert (lsu_mode = '1');
-        assert (lsu_en = '1');
-        assert (branch = '0');
-        assert (jal = '0');
-        assert (jalr = '0');
+        assert rf_write_src = "--";
+        assert rf_write_en = '0';
+        assert ig_imm_type = IMM_S_TYPE;
+        assert alu_src0 = '0';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '1';
+        assert lsu_en = '1';
+        assert br_detector_en = '0';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
 
         opcode <= BRANCH_OPCODE;
-
+            
         wait for period;
 
-        assert (rf_write_en = '0');
-        assert (rf_write_src = '0');
-        assert (lsu_mode = '0');
-        assert (lsu_en = '0');
-        assert (branch = '1');
-        assert (jal = '0');
-        assert (jalr = '0');
+        assert rf_write_src = "--";
+        assert rf_write_en = '0';
+        assert ig_imm_type = IMM_B_TYPE;
+        assert alu_src0 = '1';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '1';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
+
+        opcode <= LOAD_UPPER_IMM_OPCODE;
+            
+        wait for period;
+
+        assert rf_write_src = b"00";
+        assert rf_write_en = '1';
+        assert ig_imm_type = IMM_U_TYPE;
+        assert alu_src0 = '-';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '0';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '0';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
+
+        opcode <= ADD_UPPER_IMM_PC_OPCODE;
+            
+        wait for period;
+
+        assert rf_write_src = b"00";
+        assert rf_write_en = '1';
+        assert ig_imm_type = IMM_U_TYPE;
+        assert alu_src0 = '1';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '0';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
 
         opcode <= JAL_OPCODE;
+            
+        wait for period;
+
+        assert rf_write_src = b"10";
+        assert rf_write_en = '1';
+        assert ig_imm_type = IMM_J_TYPE;
+        assert alu_src0 = '1';
+        assert alu_src1 = '1';
+        assert alu_src0_pass = '1';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '0';
+        assert if_jmp = '1';
+        assert if_target_shift = '0';
+
+        no_op <= '1';
 
         wait for period;
 
-        assert (rf_write_en <='1');
-        assert (rf_write_src <= '0');
-        assert (lsu_mode <= '0');
-        assert (lsu_en <= '0');
-        assert (branch <= '0');
-        assert (jal <= '1');
-        assert (jalr <= '0');
+        assert rf_write_src = "--";
+        assert rf_write_en = '0';
+        assert ig_imm_type = "---";
+        assert alu_src0 = '0';
+        assert alu_src1 = '0';
+        assert alu_src0_pass = '-';
+        assert alu_la_op = '0';
+        assert alu_imm_op = '0';
+        assert lsu_mode = '-';
+        assert lsu_en = '0';
+        assert br_detector_en = '0';
+        assert if_jmp = '0';
+        assert if_target_shift = '0';
 
         wait;
         
