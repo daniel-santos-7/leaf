834584c5c7fe1b55cc837d1bd458396caf43833a
root <root@LAPTOP-C4NN74M6.localdomain>
2021-05-04 01:10:54 -0300

    alu tb changes

--- core/tbs/alu_tb.vhdl
+++ core/tbs/alu_tb.vhdl
@@ -26,55 +26,55 @@ begin
             
             op <= ALU_ADD;
             wait for period;
-            assert rslt = x"00000384" report "sum operation failed" severity failure;
+            assert rslt = x"00000384";
 
             op <= ALU_SUB;
             wait for period;
-            assert rslt = x"000000c8" report "sub operation failed" severity failure;
+            assert rslt = x"000000c8";
 
             opd0 <= x"fffffdbd";
             opd1 <= x"000004bd";
 
             op <= ALU_AND;
             wait for period;
-            assert rslt = x"000004bd" report "and operation failed" severity failure;
+            assert rslt = x"000004bd";
 
             op <= ALU_OR;
             wait for period;
-            assert rslt = x"fffffdbd" report "or operation failed" severity failure;
+            assert rslt = x"fffffdbd";
 
             op <= ALU_XOR;
             wait for period;
-            assert rslt = x"fffff900" report "xor operation failed" severity failure;
+            assert rslt = x"fffff900";
 
             opd0 <= x"000000c8";
             opd1 <= x"fffffe0c";
 
             op <= ALU_SLT;
             wait for period;
-            assert rslt = x"00000000" report "slt operation failed" severity failure;
+            assert rslt = x"00000000";
 
             op <= ALU_SLTU;
             wait for period;
-            assert rslt = x"00000001" report "sltu operation failed" severity failure;
+            assert rslt = x"00000001";
 
             opd0 <= x"00000005";
             opd1 <= x"00000021";
 
             op <= ALU_SLL;
             wait for period;
-            assert rslt = x"0000000A" report "sll operation failed" severity failure;
+            assert rslt = x"0000000a";
 
             op <= ALU_SRL;
             wait for period;
-            assert rslt = x"00000002" report "srl operation failed" severity failure;
+            assert rslt = x"00000002";
 
             opd0 <= x"00000006";
             opd1 <= x"00000003";
             
             op <= ALU_SRA;
             wait for period;
-            assert rslt = x"00000000" report "sra operation failed" severity failure;
+            assert rslt = x"00000000";
 
             wait;
 
