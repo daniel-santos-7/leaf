3d50226dfeebb7ac1a35394cb1c01d22c61e6ecf
root <root@LAPTOP-C4NN74M6.localdomain>
2021-06-01 11:08:19 -0300

    reg file refact

--- rtl/reg_file.vhdl
+++ rtl/reg_file.vhdl
@@ -5,11 +5,19 @@ use IEEE.numeric_std.all;
 entity reg_file is
 
     port (
+
         clk: in std_logic;
-        rd_reg_addr0, rd_reg_addr1, wr_reg_addr: in std_logic_vector(4 downto 0);
-        wr_reg_data: in std_logic_vector(31 downto 0);
-        wr_reg_en: in std_logic;
-        rd_reg_data0, rd_reg_data1: out std_logic_vector(31 downto 0)
+        
+        rd_reg_addr0:   in std_logic_vector(4 downto 0);
+        rd_reg_addr1:   in std_logic_vector(4 downto 0);
+        wr_reg_addr:    in std_logic_vector(4 downto 0);
+        
+        wr_reg_data:    in std_logic_vector(31 downto 0);
+        wr_reg_en:      in std_logic;
+        
+        rd_reg_data0:   out std_logic_vector(31 downto 0);
+        rd_reg_data1:   out std_logic_vector(31 downto 0)
+    
     );
 
 end entity reg_file;
@@ -18,24 +26,25 @@ architecture reg_file_arch of reg_file is
 
     type regs_array is array(0 to 31) of std_logic_vector(31 downto 0);
 
-    signal regs: regs_array := (others => x"0000_0000");
+    signal regs: regs_array;
+
+    constant X0: std_logic_vector(4 downto 0) := b"00000";
 
 begin
 
-    process(clk)
+    write_reg: process(clk)
 
     begin
         
-        if rising_edge(clk) and wr_reg_en = '1' and wr_reg_addr /= b"00000" then
+        if rising_edge(clk) and wr_reg_en = '1' and wr_reg_addr /= X0 then
                 
             regs(to_integer(unsigned(wr_reg_addr))) <= wr_reg_data;
 
         end if;
 
-    end process;
+    end process write_reg;
 
-    rd_reg_data0 <= regs(to_integer(unsigned(rd_reg_addr0)));
-			
-    rd_reg_data1 <= regs(to_integer(unsigned(rd_reg_addr1)));
+    rd_reg_data0 <= (others => '0') when rd_reg_addr0 = X0 else regs(to_integer(unsigned(rd_reg_addr0)));
+    rd_reg_data1 <= (others => '0') when rd_reg_addr1 = X0 else regs(to_integer(unsigned(rd_reg_addr1)));
 
 end architecture reg_file_arch;
--- tbs/reg_file_tb.vhdl
+++ tbs/reg_file_tb.vhdl
@@ -1,6 +1,6 @@
 library IEEE;
-use IEEE.std_logic_1164.all;
 library work;
+use IEEE.std_logic_1164.all;
 use work.core_pkg.all;
 
 entity reg_file_tb is
@@ -8,9 +8,16 @@ end entity reg_file_tb;
 
 architecture reg_file_tb_arch of reg_file_tb is
     
-    signal clk, wr_reg_en: std_logic;
-    signal rd_reg_addr0, rd_reg_addr1, wr_reg_addr: std_logic_vector(4 downto 0) := b"00000";
-    signal wr_reg_data, rd_reg_data0, rd_reg_data1: std_logic_vector(31 downto 0);
+    signal clk:       std_logic;
+    signal wr_reg_en: std_logic;
+    
+    signal rd_reg_addr0:    std_logic_vector(4 downto 0);
+    signal rd_reg_addr1:    std_logic_vector(4 downto 0);
+    signal wr_reg_addr:     std_logic_vector(4 downto 0);
+    
+    signal wr_reg_data:     std_logic_vector(31 downto 0);
+    signal rd_reg_data0:    std_logic_vector(31 downto 0);
+    signal rd_reg_data1:    std_logic_vector(31 downto 0);
 
     procedure tick(signal clk: out std_logic) is
 
@@ -18,6 +25,7 @@ architecture reg_file_tb_arch of reg_file_tb is
         
         clk <= '0';
         wait for 5 ns;
+        
         clk <= '1';
         wait for 5 ns;
 
@@ -40,35 +48,33 @@ begin
 
     begin
         
-        rd_reg_addr0 <= b"00000";
-        rd_reg_addr1 <= b"00001";
+        rd_reg_addr0    <= b"00000";
+        rd_reg_addr1    <= b"00001";
 
-        wr_reg_addr <= b"00000";
-        wr_reg_data <= x"0000ffff";
-        wr_reg_en <= '1';
-
-        tick(clk);
-
-        assert rd_reg_data0 = x"00000000" report "register reading failure" severity failure;
-        assert rd_reg_data1 = x"00000000" report "register reading failure" severity failure;
-
-        wr_reg_addr <= b"00001";
-        wr_reg_data <= x"0000ffff";
-        wr_reg_en <= '0';
+        wr_reg_addr     <= b"00000";
+        wr_reg_data     <= x"0000FFFF";
+        wr_reg_en       <= '1';
 
         tick(clk);
 
-        assert rd_reg_data0 = x"00000000" report "register write failure" severity failure;
-        assert rd_reg_data1 = x"00000000" report "register write failure" severity failure;
+        assert rd_reg_data0 = x"00000000";
 
-        wr_reg_addr <= b"00001";
-        wr_reg_data <= x"0000ffff";
-        wr_reg_en <= '1';
+        wr_reg_addr     <= b"00001";
+        wr_reg_data     <= x"0000FFFF";
+        wr_reg_en       <= '0';
 
         tick(clk);
 
-        assert rd_reg_data0 = x"00000000" report "register write failure" severity failure;
-        assert rd_reg_data1 = x"0000ffff" report "register write failure" severity failure;
+        assert rd_reg_data0 = x"00000000";
+
+        wr_reg_addr     <= b"00001";
+        wr_reg_data     <= x"0000FFFF";
+        wr_reg_en       <= '1';
+
+        tick(clk);
+
+        assert rd_reg_data0 = x"00000000";
+        assert rd_reg_data1 = x"0000FFFF";
 
         wait;
 
