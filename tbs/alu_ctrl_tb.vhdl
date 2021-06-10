e4df29abd204aada5437d7ecd2f849330a7dd35a
root <root@LAPTOP-C4NN74M6.localdomain>
2021-06-10 13:33:58 -0300

    alu and alu ctrl tbs changes

--- tbs/alu_ctrl_tb.vhdl
+++ tbs/alu_ctrl_tb.vhdl
@@ -1,54 +1,172 @@
 library IEEE;
 library work;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 use work.core_pkg.all;
 
-entity alu_ctrl_tb is 
-end alu_ctrl_tb;
+entity alu_ctrl_tb is
+end entity alu_ctrl_tb;
 
 architecture alu_ctrl_tb_arch of alu_ctrl_tb is
 
-    signal std_op, imm_op: std_logic;
-    signal func: std_logic_vector(9 downto 0);
-    signal alu_op: std_logic_vector(3 downto 0);
+    signal alu_op_en:     std_logic;
+    signal alu_func_type: std_logic;
+
+    signal func3:  std_logic_vector(2 downto 0);
+    signal func7:  std_logic_vector(6 downto 0);
+    signal alu_op: std_logic_vector(6 downto 0);
 
 begin
     
-    uut: alu_ctrl port map (std_op, imm_op, func, alu_op);
+    uut: alu_ctrl port map (
+        alu_op_en     => alu_op_en,
+        alu_func_type => alu_func_type,
+        func3         => func3,
+        func7         => func7,
+        alu_op        => alu_op
+    );
 
-    process
-
+    test: process
+        
         constant period: time := 50 ns;
 
-        begin
-
-            -- RR operations decode --
-
-            std_op <= '0';
-            imm_op <= '0';
-            func <= ALU_CTRL_SRA;
-
-            wait for period;
-            assert alu_op = ALU_SRA;
-
-            -- non-logical / arithmetic operation decode --
-
-            std_op <= '1';
-            imm_op <= '0';
-            func <= ALU_CTRL_SRA;
-
-            wait for period;
-            assert alu_op = ALU_ADD;
-
-            std_op <= '0';
-            imm_op <= '1';
-            func <= ALU_CTRL_SRA;
-
-            wait for period;
-            assert alu_op = ALU_SRA;
-
-            wait;
-
-    end process;
+    begin
+        
+        -- setup --
     
+        alu_op_en <= '1';
+
+        -- RR operations --
+
+        alu_func_type <= '0';
+
+        -- ADD operation --
+
+        func3 <= b"000";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_ADD;
+
+        -- SUB operation --
+
+        func3 <= b"000";
+        func7 <= b"0100000";
+
+        wait for period;
+
+        assert alu_op = ALU_SUB;
+
+        -- SLL operation --
+
+        func3 <= b"001";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_SLL;
+
+        -- SLT operation --
+
+        func3 <= b"010";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_SLT;
+
+        -- SLTU operation --
+
+        func3 <= b"011";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_SLTU;
+
+        -- XOR operation --
+
+        func3 <= b"100";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_XOR;
+
+        -- SRL operation --
+
+        func3 <= b"101";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_SRL;
+
+        -- SRA operation --
+
+        func3 <= b"101";
+        func7 <= b"0100000";
+
+        wait for period;
+
+        assert alu_op = ALU_SRA;
+
+        -- OR operation --
+
+        func3 <= b"110";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_OR;
+
+        -- AND operation --
+
+        func3 <= b"111";
+        func7 <= b"0000000";
+
+        wait for period;
+
+        assert alu_op = ALU_AND;
+
+        -- IMM operations --
+
+        alu_func_type <= '1';
+
+        -- ADD operation --
+
+        func3 <= b"000";
+        func7 <= b"0100000";
+
+        wait for period;
+
+        assert alu_op = ALU_ADD;
+
+        -- SRA operation --
+
+        func3 <= b"101";
+        func7 <= b"0100000";
+
+        wait for period;
+
+        assert alu_op = ALU_SRA;
+
+        -- others operations --
+
+        alu_op_en <= '0';
+
+        -- ADD operation --
+
+        func3 <= b"101";
+        func7 <= b"0100000";
+
+        wait for period;
+
+        assert alu_op = ALU_ADD;
+
+        wait;
+
+    end process test;        
+
 end architecture alu_ctrl_tb_arch;
--- tbs/alu_tb.vhdl
+++ tbs/alu_tb.vhdl
@@ -8,12 +8,19 @@ end alu_tb;
 
 architecture alu_tb_arch of alu_tb is
 
-    signal opd0, opd1, res: std_logic_vector(31 downto 0);
-    signal op : std_logic_vector(3 downto 0);
+    signal opd0: std_logic_vector(31 downto 0);
+    signal opd1: std_logic_vector(31 downto 0);
+    signal op :  std_logic_vector(5  downto 0);
+    signal res:  std_logic_vector(31 downto 0);
 
 begin
     
-    uut: alu port map (opd0, opd1, op, res);
+    uut: alu port map (
+        opd0 => opd0, 
+        opd1 => opd1, 
+        op   => op, 
+        res  => res
+    );
 
     process
 
@@ -21,72 +28,92 @@ begin
 
         begin
 
+            -- ADD operation --
+
             opd0 <= x"00000226";
             opd1 <= x"0000015E";
-            op <= ALU_ADD;
+            op   <= ALU_ADD;
 
             wait for period;
             assert res = x"00000384";
 
+            -- SUB operation --
+
             opd0 <= x"00000064";
             opd1 <= x"0000007d";
-            op <= ALU_SUB;
+            op   <= ALU_SUB;
 
             wait for period;
             assert res = x"FFFFFFE7";
 
+            -- AND operation --
+
             opd0 <= x"FFFFFDBD";
             opd1 <= x"000004BD";
-            op <= ALU_AND;
+            op   <= ALU_AND;
 
             wait for period;
             assert res = x"000004BD";
 
+            -- OR operation --
+
             opd0 <= x"00000082";
             opd1 <= x"0000015E";
-            op <= ALU_OR;
+            op   <= ALU_OR;
 
             wait for period;
             assert res = x"000001DE";
 
+            -- XOR operation --
+
             opd0 <= x"FFFFFC5E";
             opd1 <= x"00000546";
-            op <= ALU_XOR;
+            op   <= ALU_XOR;
 
             wait for period;
             assert res = x"FFFFF918";
 
+            -- SLT operation --
+
             opd0 <= x"000000C8";
             opd1 <= x"FFFFFE0C";
-            op <= ALU_SLT;
+            op   <= ALU_SLT;
 
             wait for period;
             assert res = x"00000000";
 
+            -- SLTU operation --
+
             opd0 <= x"000000C8";
             opd1 <= x"FFFFFE0C";
-            op <= ALU_SLTU;
+            op   <= ALU_SLTU;
 
             wait for period;
             assert res = x"00000001";
 
+            -- SLL operation --
+
             opd0 <= x"00000005";
             opd1 <= x"00000021";
-            op <= ALU_SLL;
+            op   <= ALU_SLL;
 
             wait for period;
             assert res = x"0000000A";
 
+            -- SRL operation --
+
             opd0 <= x"FFFFFFFE";
             opd1 <= x"00000003";
-            op <= ALU_SRL;
+            op   <= ALU_SRL;
 
             wait for period;
             assert res = x"1FFFFFFF";
 
+            -- SRA operation --
+
             opd0 <= x"FFFFFFF7";
             opd1 <= x"00000409";
-            op <= ALU_SRA;
+            op   <= ALU_SRA;
 
             wait for period;
             assert res = x"FFFFFFFF";
