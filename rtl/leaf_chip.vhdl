977ab4cd1c8bef021c6a4e2744b9e5c69b15fa02
daniel-santos-7 <danielsantos7@outlook.com.br>
2021-09-29 15:25:10 -0300

    leaf chip with uart

--- rtl/leaf_chip.vhdl
+++ rtl/leaf_chip.vhdl
@@ -1,24 +1,31 @@
+----------------------------------------------------------------------
+-- Leaf project
+-- developed by: Daniel Santos
+-- 2021
+----------------------------------------------------------------------
+
 library IEEE;
 library work;
 use IEEE.std_logic_1164.all;
 use work.leaf_chip_pkg.all;
+use work.uart_pkg.all;
 
 entity leaf_chip is
     
     port (
-        clk:   in std_logic;
-        reset: in std_logic;
-
-        sdo:  out std_logic;
-        sdi:  in  std_logic;
-        sclk: out std_logic;
-        cs:   out std_logic
+        clk:   in  std_logic;
+        reset: in  std_logic;
+        rx:    in  std_logic;
+        tx:    out std_logic;
     );
 
 end entity leaf_chip;
 
 architecture leaf_chip_arch of leaf_chip is
-    
+
+
+    ------------------------ leaf core signals ----------------------------
+
     signal core_rd_instr_mem_data: std_logic_vector(31 downto 0);
     signal core_rd_instr_mem_addr: std_logic_vector(31 downto 0);
     signal core_rd_mem_data:       std_logic_vector(31 downto 0);
@@ -31,8 +38,18 @@ architecture leaf_chip_arch of leaf_chip is
     signal core_sw_irq:            std_logic;
     signal core_tm_irq:            std_logic;
 
+    ----------------------------------------------------------------------
+
+
+    --------------------------- ROM signals ------------------------------
+
     signal rom_rd_data: std_logic_vector(31 downto 0);
     signal rom_rd_addr: std_logic_vector(5 downto 0);
+
+    ----------------------------------------------------------------------
+
+
+    --------------------------- RAM signals ------------------------------
 
     signal ram_rd_addr0:   std_logic_vector(5  downto 0);
     signal ram_rd_data0:   std_logic_vector(31 downto 0);
@@ -43,16 +60,32 @@ architecture leaf_chip_arch of leaf_chip is
     signal ram_wr_byte_en: std_logic_vector(3  downto 0);
     signal ram_wr_en:      std_logic;
 
-    signal spi_rd_addr:    std_logic_vector(1  downto 0);
-    signal spi_rd_data:    std_logic_vector(31 downto 0);
-    signal spi_wr_addr:    std_logic_vector(1  downto 0);
-    signal spi_wr_data:    std_logic_vector(31 downto 0);
-    signal spi_wr_byte_en: std_logic_vector(3 downto 0);
-    signal spi_wr_en:      std_logic;
+    ----------------------------------------------------------------------
+
+
+    --------------------------- RAM signals ------------------------------
+
+    signal uart_rd_addr:    std_logic_vector(1  downto 0);
+    signal uart_rd_data:    std_logic_vector(31 downto 0);
+    signal uart_wr_addr:    std_logic_vector(1  downto 0);
+    signal uart_wr_data:    std_logic_vector(31 downto 0);
+    signal uart_wr_byte_en: std_logic_vector(3 downto 0);
+    signal uart_wr_en:      std_logic;
+    signal uart_rd_en:      std_logic;
+
+    ----------------------------------------------------------------------
+
 
 begin
     
-    instr_arbiter: process(core_rd_instr_mem_addr, rom_rd_data, ram_rd_data0)
+    --------------------- read instructions ----------------------------
+
+    --
+    --      0x00000000 - 0x00000100 : ROM
+    --      0x00000100 - 0x00000200 : RAM
+    --
+
+    read_instruction: process(core_rd_instr_mem_addr, rom_rd_data, ram_rd_data0)
    
         variable base_addr: std_logic_vector(23 downto 0);
 
@@ -60,109 +93,133 @@ begin
    
         base_addr := core_rd_instr_mem_addr(31 downto 8);
 
-        if base_addr = x"000000" then
+        case base_addr is
             
-            core_rd_instr_mem_data <= x"00000013";
+            when x"000001" => 
 
-        elsif base_addr = x"000001" then
+                core_rd_instr_mem_data <= rom_rd_data;
 
-            core_rd_instr_mem_data <= rom_rd_data;
+            when x"000002" => 
 
-        elsif base_addr = x"000002" then
+                core_rd_instr_mem_data <= ram_rd_data0;
 
-            core_rd_instr_mem_data <= ram_rd_data0;
+            when others => 
+
+                core_rd_instr_mem_data <= x"00000013";
+                
+        end case;
+
+    end process read_instruction;
+
+    ----------------------------------------------------------------------
+
+
+    --------------------------- read data --------------------------------
+    
+    --
+    --  0x00000000 - 0x00000100 : UART
+    --  0x00000100 - 0x00000200 : ROM
+    --  0x00000200 - 0x00000300 : RAM
+    --
+
+    read_data: process(core_rd_wr_mem_addr, core_rd_mem_en, uart_rd_data, rom_rd_data, ram_rd_data1)
+
+        variable base_addr: std_logic_vector(23 downto 0);
+
+    begin
+
+        base_addr := core_rd_wr_mem_addr(31 downto 8);
+
+        if core_rd_mem_en = '1' then
+
+            case base_addr is
+                
+                when x"000000" =>
+                    
+                    core_rd_mem_data <= uart_rd_data;
+                    uart_rd_en <= '1';
+            
+                when x"000001" =>
+
+                    core_rd_mem_data <= rom_rd_data;
+                    uart_rd_en <= '0';
+
+
+                when x"000002" =>
+
+                    core_rd_mem_data <= ram_rd_data1;
+                    uart_rd_en <= '0';
+
+                when others =>
+                    
+                    core_rd_mem_data <= (others => '0');
+                    uart_rd_en <= '0';
+            
+            end case;
 
         else
 
-            core_rd_instr_mem_data <= x"00000013";
+            core_rd_mem_data <= (others => '0');
+            uart_rd_en <= '0';
 
         end if;
+        
+    end process read_data;
 
-    end process instr_arbiter;
+    ----------------------------------------------------------------------
 
-    data_arbiter: process(core_rd_wr_mem_addr, core_rd_mem_en, core_wr_mem_en, spi_rd_data, rom_rd_data, ram_rd_data1)
+
+    -------------------------- write data --------------------------------
+    
+    --
+    --  0x00000000 - 0x00000100 : UART
+    --  0x00000200 - 0x00000300 : RAM
+    --
+
+    data_write: process(core_rd_wr_mem_addr, core_wr_mem_en)
     
         variable base_addr: std_logic_vector(23 downto 0);
 
     begin
 
         base_addr := core_rd_wr_mem_addr(31 downto 8);
-    
-        if core_rd_mem_en = '1' then
-            
-            if base_addr = x"000000" then
-            
-                core_rd_mem_data <= spi_rd_data;
-
-            elsif base_addr = x"000001" then
-    
-                core_rd_mem_data <= rom_rd_data;
-    
-            elsif base_addr = x"000002" then
-    
-                core_rd_mem_data <= ram_rd_data1;
-    
-            else
-    
-                core_rd_mem_data <= (others => '0');
-    
-            end if;
-
-        else
-
-            core_rd_mem_data <= (others => '0');
-
-        end if;
 
         if core_wr_mem_en = '1' then
             
-            if base_addr = x"000000" then
+            case base_addr is
                 
-                ram_wr_en <= '0';
-                spi_wr_en <= '1';
+                when x"000000" =>
+                    
+                    ram_wr_en  <= '0';
+                    uart_wr_en <= '1';
+            
+                when x"000002" =>
+                    
+                    ram_wr_en  <= '1';
+                    uart_wr_en <= '0';
 
-            elsif base_addr = x"000001" then
-
-                ram_wr_en <= '0';
-                spi_wr_en <= '0';
-
-            elsif base_addr = x"000002" then
-
-                ram_wr_en <= '1';
-                spi_wr_en <= '0';
-
-            else
-
-                ram_wr_en <= '0';
-                spi_wr_en <= '0';
-
-            end if;
+                when others =>
+                    
+                    ram_wr_en  <= '0';
+                    uart_wr_en <= '0';
+            
+            end case;
 
         else
 
-            ram_wr_en <= '0';
-            spi_wr_en <= '0';
+            ram_wr_en  <= '0';
+            uart_wr_en <= '0';
 
         end if;
         
-    end process data_arbiter;
+    end process data_write;
+
+    ----------------------------------------------------------------------
+
+
+    -------------------------- ROM memory --------------------------------
 
     rom_rd_addr <= core_rd_instr_mem_addr(7 downto 2);
-
-    ram_rd_addr0   <= core_rd_instr_mem_addr(7 downto 2);
-    ram_rd_addr1   <= core_rd_wr_mem_addr(7 downto 2);
-    ram_wr_addr    <= core_rd_wr_mem_addr(7 downto 2);
-    ram_wr_data    <= core_wr_mem_data;
-    ram_wr_byte_en <= core_wr_mem_byte_en;
-
-    spi_rd_addr    <= core_rd_wr_mem_addr(3 downto 2);
-    spi_wr_data    <= core_wr_mem_data;
-    spi_wr_addr    <= core_rd_wr_mem_addr(3 downto 2);
-    spi_wr_byte_en <= core_wr_mem_byte_en;
-
-    core_ex_irq <= '0';
-    core_sw_irq <= '0';
-    core_tm_irq <= '0';
 
     leaf_rom: rom generic map (
         MEM_SIZE  => 256,
@@ -171,6 +228,17 @@ begin
         rd_addr => rom_rd_addr,
         rd_data => rom_rd_data
     );
+
+    ----------------------------------------------------------------------
+
+
+    -------------------------- RAM memory --------------------------------
+
+    ram_rd_addr0   <= core_rd_instr_mem_addr(7 downto 2);
+    ram_rd_addr1   <= core_rd_wr_mem_addr(7 downto 2);
+    ram_wr_addr    <= core_rd_wr_mem_addr(7 downto 2);
+    ram_wr_data    <= core_wr_mem_data;
+    ram_wr_byte_en <= core_wr_mem_byte_en;
     
     leaf_ram: ram generic map (
         MEM_SIZE  => 256,
@@ -187,20 +255,40 @@ begin
         wr_en       => ram_wr_en
     );
 
-    leaf_spi: spi port map (
+    ----------------------------------------------------------------------
+
+
+    -------------------------- uart module -------------------------------
+
+    uart_wr_data    <= core_wr_mem_data;
+    uart_rd_addr    <= core_rd_wr_mem_addr(3 downto 2);
+    uart_wr_addr    <= core_rd_wr_mem_addr(3 downto 2);
+    uart_wr_byte_en <= core_wr_mem_byte_en;
+
+    leaf_uart: generic map(
+        UART_BAUD: integer
+    ) port map(
         clk        => clk,
         reset      => reset,
-        rd_addr    => spi_rd_addr,
-        rd_data    => spi_rd_data,
-        wr_addr    => spi_wr_addr,
-        wr_data    => spi_wr_data,
-        wr_byte_en => spi_wr_byte_en,
-        wr_en      => spi_wr_en,
-        sdo        => sdo,
-        sdi        => sdi,
-        sclk       => sclk,
-        cs         => cs
+        rd_en      => uart_rd_en,
+        rd_addr    => uart_rd_addr,
+        rd_data    => uart_rd_data,
+        wr_en      => uart_wr_en,
+        wr_addr    => uart_wr_addr
+        wr_data    => uart_wr_data,
+        wr_byte_en => uart_wr_byte_en
+        rx         => rx,
+        tx         => tx
     );
+
+    ----------------------------------------------------------------------
+
+
+    ---------------------------- leaf core -------------------------------
+
+    core_ex_irq <= '0';
+    core_sw_irq <= '0';
+    core_tm_irq <= '0';
 
     leaf_core: core generic map (
         RESET_ADDR => x"00000100"
@@ -219,5 +307,8 @@ begin
         sw_irq            => core_sw_irq,
         tm_irq            => core_tm_irq
     );
+
+    ----------------------------------------------------------------------
+
 
 end architecture leaf_chip_arch;
--- rtl/uart/uart_pkg.vhdl
+++ rtl/uart/uart_pkg.vhdl
@@ -31,5 +31,28 @@ package uart_pkg is
             rd_en:   out std_logic
         );
     end component uart_rx;
+
+    component uart is
+        generic (
+            UART_BAUD: integer
+        );
+    
+        port (
+            clk:   in  std_logic;
+            reset: in  std_logic;
+            
+            rd_en:   in  std_logic;
+            rd_addr: in  std_logic_vector(1  downto 0);
+            rd_data: out std_logic_vector(31 downto 0);
+    
+            wr_en:      in std_logic;
+            wr_addr:    in std_logic_vector(1  downto 0);
+            wr_data:    in std_logic_vector(31 downto 0);
+            wr_byte_en: in std_logic_vector(3  downto 0);
+    
+            rx: in  std_logic;
+            tx: out std_logic
+        );
+    end component uart;
     
 end package uart_pkg;
