8bc05b85aa30099de57e07f234b4b742ed156c10
daniel-santos-7 <danielsantos7@outlook.com.br>
2021-10-02 09:24:05 -0300

    new leaf_chip_tb

--- rtl/leaf_chip_tb.vhdl
+++ rtl/leaf_chip_tb.vhdl
@@ -11,14 +11,14 @@ architecture leaf_chip_tb_arch of leaf_chip_tb is
     signal clk:   std_logic;
     signal reset: std_logic;
 
-    signal sdo:  std_logic;
-    signal sdi:  std_logic;
-    signal sclk: std_logic;
-    signal cs:   std_logic;
+    signal rx:  std_logic;
+    signal tx:  std_logic;
+
+    constant period: time := 20 ns;
 
     type program is array (natural range<>) of std_logic_vector(31 downto 0);
 
-    constant hello_program: program := (
+    constant test_program: program := (
         0 =>	x"00000013",
         1 =>	x"04800593",
         2 =>	x"024000ef",
@@ -38,120 +38,61 @@ architecture leaf_chip_tb_arch of leaf_chip_tb is
         16 =>	x"00008067"
     );
 
-    signal slave_data:  std_logic_vector(31 downto 0) := (others => '1');
-
-    procedure tick(signal clk: out std_logic) is
-
-    begin
-        
-        clk <= '0';
-        wait for 500 ns;
-        
-        clk <= '1';
-        wait for 500 ns;
-
-    end procedure;
-
 begin
 
     uut: leaf_chip port map (
-        clk        => clk,
-        reset      => reset,
-        sdo        => sdo,
-        sdi        => sdi,
-        sclk       => sclk,
-        cs         => cs
+        clk   => clk,
+        reset => reset,
+        rx    => rx,
+        tx    => tx
     );
 
-    spi_slave: process(cs, sclk)
-        
-        variable instr_count: integer := 0;
-        variable bit_count:   integer := 0;
-
-    begin
-
-        if cs = '0' then
-            
-            if rising_edge(sclk) then
-            
-                sdi <= slave_data(31);
-
-                bit_count := bit_count + 1;
-
-            end if;
-    
-            if falling_edge(sclk) then
-                
-                slave_data <= slave_data(30 downto 0) & sdo;
-
-            end if;
-
-        else
-
-            if bit_count = 32 then
-                
-                if instr_count < hello_program'length then
-                
-                    slave_data <= hello_program(instr_count);
-    
-                else
-    
-                    slave_data <= (others => '1');
-    
-                end if;
-
-                instr_count := instr_count + 1;
-
-                bit_count := 0;
-
-            end if;
-
-            sdi <= '0';
-
-        end if;
-
-    end process spi_slave;
-
     test: process
-    
+
+        constant load_cmd: std_logic_vector(31 downto 0) := x"00000077";
+        constant uart_baud: integer := 5208;
+        variable tx_frame: std_logic_vector(9 downto 0);
+
     begin
         
-        clk     <= '0';
-        reset   <= '1';
-        
-        tick(clk);
+        reset <= '1';
+        rx    <= '1';
+
+        clk <= '0';
+        wait for period/2;
+
+        clk <= '1';
+        wait for period/2;
 
         reset <= '0';
+      
+        tx_frame := '1' & load_cmd(7 downto 0) & '0';
 
-        tick(clk);
+        for i in 0 to 9 loop
+        
+            rx <= tx_frame(i);
 
-        while slave_data /=x"00000048"  loop
+            for j in 0 to 2*uart_baud loop
+                
+            clk <= not clk;
+            wait for period/2;
 
-            tick(clk);
-
-        end loop;
-
-        while slave_data /=x"00000045"  loop
-
-            tick(clk);
-
-        end loop;
-
-        while slave_data /=x"0000004C"  loop
-
-            tick(clk);
+            end loop;
 
         end loop;
 
-        while slave_data /=x"0000004C"  loop
+        tx_frame := '1' & load_cmd(15 downto 8) & '0';
 
-            tick(clk);
+        for i in 0 to 9 loop
+        
+            rx <= tx_frame(i);
 
-        end loop;
+            for j in 0 to 2*uart_baud loop
+                
+            clk <= not clk;
+            wait for period/2;
 
-        while slave_data /=x"0000004F"  loop
-
-            tick(clk);
+            end loop;
 
         end loop;
 
