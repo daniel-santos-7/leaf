cfd3652926e9cd7b3a46485bf19857d03d6a70de
daniel-santos-7 <danielsantos7@outlook.com.br>
2022-01-23 18:14:08 -0300

    ex_block simplified interface

--- rtl/ex_block.vhdl
+++ rtl/ex_block.vhdl
@@ -1,41 +1,58 @@
 library IEEE;
+library work;
 use IEEE.std_logic_1164.all;
 use work.core_pkg.all;
 
 entity ex_block is
     port (
-        opd0_src0:    in  std_logic_vector(31 downto 0);
-        opd0_src1:    in  std_logic_vector(31 downto 0);
-        opd1_src0:    in  std_logic_vector(31 downto 0);
-        opd1_src1:    in  std_logic_vector(31 downto 0);
-        opd0_src_sel: in  std_logic;
-        opd1_src_sel: in  std_logic;
-        opd0_pass:    in  std_logic;
-        opd1_pass:    in  std_logic;
-        func_type:    in  std_logic;
-        op_en:        in  std_logic;
-        func3:        in  std_logic_vector(2  downto 0);
-        func7:        in  std_logic_vector(6  downto 0);
-        res:          out std_logic_vector(31 downto 0)
+        opd0_src0: in  std_logic_vector(31 downto 0);
+        opd0_src1: in  std_logic_vector(31 downto 0);
+        opd1_src0: in  std_logic_vector(31 downto 0);
+        opd1_src1: in  std_logic_vector(31 downto 0);
+        ex_ctrl:   in  std_logic_vector(5  downto 0);     
+        ex_func:   in  std_logic_vector(9  downto 0);
+        res:       out std_logic_vector(31 downto 0)
     );
 end entity ex_block;
 
 architecture ex_block_arch of ex_block is
     
-    signal opd0:     std_logic_vector(31 downto 0);
-    signal opd1:     std_logic_vector(31 downto 0);
+    signal op_en:        std_logic;
+    signal func_type:    std_logic;
+    signal opd0_src_sel: std_logic;
+    signal opd1_src_sel: std_logic;
+    signal opd0_pass:    std_logic;
+    signal opd1_pass:    std_logic;
+
+    signal func3: std_logic_vector(2 downto 0);
+    signal func7: std_logic_vector(6 downto 0);
+
+    signal opd0: std_logic_vector(31 downto 0);
+    signal opd1: std_logic_vector(31 downto 0);
+    
     signal gtd_opd0: std_logic_vector(31 downto 0);
     signal gtd_opd1: std_logic_vector(31 downto 0);
-    signal op:       std_logic_vector(5  downto 0);
+    
+    signal op: std_logic_vector(5 downto 0);
 
 begin
+
+    op_en        <= ex_ctrl(0);
+    func_type    <= ex_ctrl(1);
+    opd0_pass    <= ex_ctrl(2);
+    opd1_pass    <= ex_ctrl(3);
+    opd0_src_sel <= ex_ctrl(4);
+    opd1_src_sel <= ex_ctrl(5);
+
+    func3 <= ex_func(2 downto 0);
+    func7 <= ex_func(9 downto 3);
 
     opd0 <= opd0_src1 when opd0_src_sel = '1' else opd0_src0;
     opd1 <= opd1_src1 when opd1_src_sel = '1' else opd1_src0;
 
-    gtd_opd0 <= opd0 when opd0_pass = '1' else (others => '0');
-    gtd_opd1 <= opd1 when opd1_pass = '1' else (others => '0');
-    
+    gtd_opd0 <= opd0 and (31 downto 0 => opd0_pass);
+    gtd_opd1 <= opd1 and (31 downto 0 => opd1_pass);
+
     ex_alu_ctrl: alu_ctrl port map (
         alu_op_en     => op_en,
         alu_func_type => func_type,
--- tbs/ex_block_tb.vhdl
+++ tbs/ex_block_tb.vhdl
@@ -8,36 +8,24 @@ end ex_block_tb;
 
 architecture ex_block_tb_arch of ex_block_tb is
 
-    signal opd0_src0:    std_logic_vector(31 downto 0);
-    signal opd0_src1:    std_logic_vector(31 downto 0);
-    signal opd1_src0:    std_logic_vector(31 downto 0);
-    signal opd1_src1:    std_logic_vector(31 downto 0);
-    signal opd0_src_sel: std_logic;
-    signal opd1_src_sel: std_logic;
-    signal opd0_pass:    std_logic;
-    signal opd1_pass:    std_logic;
-    signal func_type:    std_logic;
-    signal op_en:        std_logic;
-    signal func3:        std_logic_vector(2  downto 0);
-    signal func7:        std_logic_vector(6  downto 0);
-    signal res:          std_logic_vector(31 downto 0);
+    signal opd0_src0: std_logic_vector(31 downto 0);
+    signal opd0_src1: std_logic_vector(31 downto 0);
+    signal opd1_src0: std_logic_vector(31 downto 0);
+    signal opd1_src1: std_logic_vector(31 downto 0);
+    signal ex_ctrl:   std_logic_vector(5  downto 0);
+    signal ex_func:   std_logic_vector(9  downto 0);
+    signal res:       std_logic_vector(31 downto 0);
 
 begin
     
     uut: ex_block port map (
-        opd0_src0    => opd0_src0,
-        opd0_src1    => opd0_src1,
-        opd1_src0    => opd1_src0,
-        opd1_src1    => opd1_src1,
-        opd0_src_sel => opd0_src_sel,
-        opd1_src_sel => opd1_src_sel,
-        opd0_pass    => opd0_pass,
-        opd1_pass    => opd1_pass,
-        func_type    => func_type,
-        op_en        => op_en,
-        func3        => func3,
-        func7        => func7,
-        res          => res
+        opd0_src0 => opd0_src0,
+        opd0_src1 => opd0_src1,
+        opd1_src0 => opd1_src0,
+        opd1_src1 => opd1_src1,
+        ex_ctrl   => ex_ctrl,
+        ex_func   => ex_func,
+        res       => res
     );
 
     test: process
@@ -46,20 +34,14 @@ begin
 
         begin
 
-            opd0_src0    <= (others => '0');
-            opd0_src1    <= (others => '0');
-            opd1_src0    <= (others => '0');
-            opd1_src1    <= (others => '0');
-            opd0_src_sel <= '0';
-            opd1_src_sel <= '0';
-            opd0_pass    <= '0';
-            opd1_pass    <= '0';
-            func_type    <= '0';
-            func3        <= (others => '0');
-            func7        <= (others => '0');
+            opd0_src0 <= (others => '0');
+            opd0_src1 <= (others => '0');
+            opd1_src0 <= (others => '0');
+            opd1_src1 <= (others => '0');
+            ex_ctrl   <= (others => '0');
+            ex_func   <= (others => '0');
 
             wait for period;
-
             wait;
 
     end process test;
