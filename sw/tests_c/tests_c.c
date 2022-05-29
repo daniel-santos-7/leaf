0efef2cb2553e602248e9c098344696fd1573e58
daniel-santos-7 <danielsantos7@outlook.com.br>
2022-05-29 12:06:16 -0300

    c test modified

--- sw/tests_c/tests_c.c
+++ sw/tests_c/tests_c.c
@@ -1,23 +1,6 @@
-#include <string.h>
-
-#define OUTPUT_ADDR 0x00200000
-#define HALT_ADDR 0x00200004
-
-void send_string(const char* str) {
-  char volatile *out = (char *) OUTPUT_ADDR;
-  int n = strlen(str);
-  for(int i=0; i<n; i++) {
-    (*out) = str[i];
-  }
-}
-
-void send_char(const char c) {
-  char volatile *out = (char *) OUTPUT_ADDR;
-  (*out) = c;
-}
+#include <stdio.h>
 
 int main() {
-  char * name = "Leaf";
-  send_string(name);
+  printf("Hello World!\n");
   return 0;
 }
