25cc8f067e9198a7afb06b8f1b7157de53b83be3
daniel-santos-7 <danielsantos7@outlook.com.br>
2022-08-25 11:07:18 -0300

    tests_c modified

--- sw/tests_c/main.c
+++ sw/tests_c/main.c
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 
 int main() {
-  int arr[] = {0, 23, 14, 12, 9};
+  // int arr[] = {0, 23, 14, 12, 9};
 
-  int len = sizeof(arr)/sizeof(int);
+  // int len = sizeof(arr)/sizeof(int);
 
-  int tmp;
+  // int tmp;
 
-  for (int i=0; i<len;i++) {
-    for (int j=i; j<len; j++) {
-      if (arr[i] > arr[j]) {
-        tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-      }
-    }
-  }
+  // for (int i=0; i<len;i++) {
+  //   for (int j=i; j<len; j++) {
+  //     if (arr[i] > arr[j]) {
+  //       tmp = arr[i];
+  //       arr[i] = arr[j];
+  //       arr[j] = tmp;
+  //     }
+  //   }
+  // }
 
-  for (int i=0; i<len; i++) {
-    printf("%d ", arr[i]);
-  }
+  // for (int i=0; i<len; i++) {
+  //   printf("%d ", arr[i]);
+  // }
 
-  printf("\n");
+  // printf("\n");
+  printf("%f\n", 3.1415);
   return 0;
 }
