6442d7a6fc43bbdb481a7130d37b021778e76470
daniel-santos-7 <danielsantos7@outlook.com.br>
2022-09-12 23:54:18 -0300

    ram refactoring

--- soc/rtl/ram.vhdl
+++ soc/rtl/ram.vhdl
@@ -1,3 +1,10 @@
+----------------------------------------------------------------------
+-- Leaf project
+-- developed by: Daniel Santos
+-- module: ram with wishbone interface
+-- 2022
+----------------------------------------------------------------------
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
@@ -24,89 +31,94 @@ architecture rtl of ram is
     
     constant MEM_SIZE: natural := 2**BITS;
 
-    type byte_array is array (0 to MEM_SIZE/4-1) of std_logic_vector(7 downto 0);
+    type mem_array is array (0 to MEM_SIZE/4-1) of std_logic_vector(7 downto 0);
 
-    signal mem0: byte_array;
-    signal mem1: byte_array;
-    signal mem2: byte_array;
-    signal mem3: byte_array;
+    signal mem0 : mem_array;
+    signal mem1 : mem_array;
+    signal mem2 : mem_array;
+    signal mem3 : mem_array;
 
-    type state is (START, IDLE, ACKNOWLEDGEMENT);
+    signal ack : std_logic;
+    
+    signal mem0_ack : std_logic;
+    signal mem1_ack : std_logic;
+    signal mem2_ack : std_logic;
+    signal mem3_ack : std_logic;
 
-    signal curr_state : state;
-    signal next_state : state;
+    signal mem0_we : std_logic;
+    signal mem1_we : std_logic;
+    signal mem2_we : std_logic;
+    signal mem3_we : std_logic;
+
+    signal mem0_en : std_logic;
+    signal mem1_en : std_logic;
+    signal mem2_en : std_logic;
+    signal mem3_en : std_logic;
 
 begin
-    
-    fsm: process(clk_i)
-    begin
-        if rising_edge(clk_i) then
-            if rst_i = '1' then
-                curr_state <= START;
-            else
-                curr_state <= next_state;
-            end if;
-        end if;
-    end process fsm;
 
-    fsm_next_state: process(curr_state, cyc_i, stb_i)
-    begin
-        case curr_state is
-            when START =>
-                next_state <= IDLE;
-            when IDLE =>
-                if cyc_i = '1' and stb_i = '1' then
-                    next_state <= ACKNOWLEDGEMENT;
-                else
-                    next_state <= IDLE;
-                end if;
-            when ACKNOWLEDGEMENT =>
-                if cyc_i = '1' and stb_i = '1' then
-                    next_state <= IDLE;
-                else
-                    next_state <= ACKNOWLEDGEMENT;
-                end if;
-        end case;
-    end process fsm_next_state;
+    mem0_ack <= not ack and cyc_i and stb_i and sel_i(0);
+    mem1_ack <= not ack and cyc_i and stb_i and sel_i(1);
+    mem2_ack <= not ack and cyc_i and stb_i and sel_i(2);
+    mem3_ack <= not ack and cyc_i and stb_i and sel_i(3);
+
+    mem0_we <= mem0_ack and we_i;
+    mem1_we <= mem1_ack and we_i;
+    mem2_we <= mem2_ack and we_i;
+    mem3_we <= mem3_ack and we_i;
+
+    mem0_en <= mem0_ack and not we_i;
+    mem1_en <= mem1_ack and not we_i;
+    mem2_en <= mem2_ack and not we_i;
+    mem3_en <= mem3_ack and not we_i;
 
     main: process(clk_i)
         variable addr: integer range 0 to MEM_SIZE/4-1;
     begin
         if rising_edge(clk_i) then
             addr := to_integer(unsigned(adr_i));
-            if next_state = ACKNOWLEDGEMENT then
-                if sel_i(0) = '1' then
-                    if we_i = '1' then
-                        mem0(addr) <= dat_i(7  downto 0);
-                    else
-                        dat_o(7  downto  0) <= mem0(addr);
-                    end if;
-                end if;
-                if sel_i(1) = '1' then
-                    if we_i = '1' then
-                        mem1(addr) <= dat_i(15 downto 8);
-                    else
-                        dat_o(15 downto  8) <= mem1(addr);
-                    end if;
-                end if;
-                if sel_i(2) = '1' then
-                    if we_i = '1' then
-                        mem2(addr) <= dat_i(23 downto 16);
-                    else
-                        dat_o(23 downto 16) <= mem2(addr);
-                    end if;
-                end if;
-                if sel_i(3) = '1' then
-                    if we_i = '1' then
-                        mem3(addr) <= dat_i(31 downto 24);
-                    else
-                        dat_o(31 downto 24) <= mem3(addr);
-                    end if;
-                end if;
+
+            if mem0_we = '1' then
+                mem0(addr) <= dat_i(7 downto 0);
+            end if;
+            if mem1_we = '1' then
+                mem1(addr) <= dat_i(15 downto 8);
+            end if;
+            if mem2_we = '1' then
+                mem2(addr) <= dat_i(23 downto 16);
+            end if;
+            if mem3_we = '1' then
+                mem3(addr) <= dat_i(31 downto 24);
+            end if;
+
+            if mem0_en = '1' then
+                dat_o(7 downto 0) <= mem0(addr);
+            end if;
+            if mem1_en = '1' then
+                dat_o(15 downto  8) <= mem1(addr);
+            end if;
+            if mem2_en = '1' then
+                dat_o(23 downto 16) <= mem2(addr);
+            end if;
+            if mem3_en = '1' then
+                dat_o(31 downto 24) <= mem3(addr);
             end if;
         end if;
     end process main;
 
-    ack_o <= '1' when curr_state = ACKNOWLEDGEMENT else '0';
+    ack_reg: process(clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if rst_i = '1' then
+                ack <= '0';
+            elsif ack = '1' then
+                ack <= not (cyc_i and stb_i);
+            else
+                ack <= cyc_i and stb_i;
+            end if;
+        end if;
+    end process ack_reg;
+
+    ack_o <= ack;
 
 end architecture rtl;
