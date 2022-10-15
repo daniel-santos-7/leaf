fc610820cb3bdd14bf118fc64e3d510b340a7072
daniel-santos-7 <danielsantos7@outlook.com.br>
2022-10-14 22:20:29 -0300

    upload.sh modified

--- sw/utils/upload.sh
+++ sw/utils/upload.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+chmod 777 /dev/ttyUSB0
+
 stty -F /dev/ttyUSB0 speed 9600 cs8 -cstopb -parenb
+
+echo "sending data ..."
 
 echo '1' > /dev/ttyUSB0
 
 cat $1 /dev/zero | head -c64k > /dev/ttyUSB0
+
+echo "OK"
+
+cat /dev/ttyUSB0
