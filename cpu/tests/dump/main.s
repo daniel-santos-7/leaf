2829749b9ec4bc5f8ca6289751e57b8280343fa5
Daniel Santos <danielsantos7@outlook.com.br>
2025-12-27 21:02:40 -0300

    dump test update

--- cpu/tests/dump/main.s
+++ cpu/tests/dump/main.s
@@ -1,23 +1,15 @@
-.equ MEM_SIZE, 0x1000
-
-.equ DUMP_START_ADDR, MEM_SIZE - 0xC
-.equ DUMP_LENGTH_ADDR, MEM_SIZE - 0x8
-.equ HALT_CMD_ADDR, MEM_SIZE - 0x4
-
-.equ HALT_CMD_DATA, 0xDEADBEEF
+.include "defs.inc"
 
 .globl _start
 _start:
-    li t0, MEM_SIZE
+    li t0, DUMP_START_ADDR
+    li t1, 0x0
+    sw t1, 0x0(t0)
 
-    li t1, 0xCAFEBABE
-    sw t1, -16(t0)
+    li t0, DUMP_STOP_ADDR
+    li t1, MEM_SIZE-1
+    sw t1, 0x0(t0)
 
-    li t1, 0xFF0
-    sw t1, -12(t0)
-
-    li t1, 0xFFF
-    sw t1, -8(t0)
-
+    li t0, HALT_CMD_ADDR
     li t1, HALT_CMD_DATA
-    sw t1, -4(t0)
+    sw t1, 0x0(t0)
