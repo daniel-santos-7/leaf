de7b2cb29e9aa2875609ed4dbe8a62736e6bd723
Daniel Santos <danielsantos7@outlook.com.br>
2026-02-04 23:46:53 -0300

    Add leaf_pkg VHDL package with constant XLEN for data path length

---
+++ cpu/rtl/leaf_pkg.vhdl
@@ -0,0 +1,16 @@
+----------------------------------------------------------------------
+-- Leaf project
+-- developed by: Daniel Santos
+-- package: leaf package
+-- 2022
+----------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+package leaf_pkg is
+
+    -- Data path length --
+    constant XLEN : natural := 32;
+    
+end package leaf_pkg;
