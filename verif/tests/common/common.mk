38e493c221a0a2be9eeb7c29577c07b2c2e16780
Daniel Santos <danielsantos7@outlook.com.br>
2026-04-05 13:32:16 -0300

    Refactor shared Leaf and Spike test runtime

--- verif/tests/common/common.mk
+++ verif/tests/common/common.mk
@@ -5,11 +5,12 @@ RISCV_OBJCOPY ?= $(RISCV_PREFIX)-objcopy
 
 MARCH ?= rv32i
 MABI  ?= ilp32
+DUMP_SIZE ?= 128
 
 COMMON_DIR ?= $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
-RISCV_GCC_OPTS ?= -nostartfiles -nostdlib -Ttext 0x80000000 -march=$(MARCH) -mabi=$(MABI) -O0 -g -I$(COMMON_DIR)
+RISCV_GCC_OPTS ?= -nostartfiles -nostdlib -march=$(MARCH) -mabi=$(MABI) -O0 -g -I$(COMMON_DIR) -Wl,--defsym=DUMP_SIZE=$(DUMP_SIZE)
 
-APP_SRC ?= $(wildcard *.s) $(wildcard *.S) $(COMMON_DIR)/common.S
+APP_SRC ?= $(wildcard *.s) $(wildcard *.S)
 APP_OUT ?= out
 
 .PHONY: run all clean
@@ -17,16 +18,26 @@ APP_OUT ?= out
 run: $(APP_OUT).bin
 	@$(MAKE) -C ../../../ run PROGRAM=$(CURDIR)/$< DUMP_FILE=$(CURDIR)/$(APP_OUT).dump
 
-all: $(APP_OUT).elf $(APP_OUT).bin $(APP_OUT).debug
+all: $(APP_OUT).elf $(APP_OUT).bin $(APP_OUT).debug $(APP_OUT).spike.elf $(APP_OUT).spike.bin $(APP_OUT).spike.debug
 
 clean:
 	@rm -f $(APP_OUT).elf $(APP_OUT).bin $(APP_OUT).debug
+	@rm -f $(APP_OUT).spike.elf $(APP_OUT).spike.bin $(APP_OUT).spike.debug
 
-$(APP_OUT).elf: $(APP_SRC)
-	$(RISCV_GCC) $(RISCV_GCC_OPTS) $^ -o $@
+$(APP_OUT).elf: $(APP_SRC) $(COMMON_DIR)/common.S $(COMMON_DIR)/leaf.S $(COMMON_DIR)/leaf.ld
+	$(RISCV_GCC) $(RISCV_GCC_OPTS) -T $(COMMON_DIR)/leaf.ld $(APP_SRC) $(COMMON_DIR)/common.S $(COMMON_DIR)/leaf.S -o $@
 
 $(APP_OUT).bin: $(APP_OUT).elf
 	$(RISCV_OBJCOPY) -O binary $^ $@
 
 $(APP_OUT).debug: $(APP_OUT).elf
+	$(RISCV_OBJDUMP) $^ --source > $@
+
+$(APP_OUT).spike.elf: $(APP_SRC) $(COMMON_DIR)/common.S $(COMMON_DIR)/spike.S $(COMMON_DIR)/spike.ld
+	$(RISCV_GCC) $(RISCV_GCC_OPTS) -T $(COMMON_DIR)/spike.ld $(APP_SRC) $(COMMON_DIR)/common.S $(COMMON_DIR)/spike.S -o $@
+
+$(APP_OUT).spike.bin: $(APP_OUT).spike.elf
+	$(RISCV_OBJCOPY) -O binary $^ $@
+
+$(APP_OUT).spike.debug: $(APP_OUT).spike.elf
 	$(RISCV_OBJDUMP) $^ --source > $@
