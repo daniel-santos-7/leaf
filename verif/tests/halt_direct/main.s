e03af277e7ca02728f14fe9ebb17f93720db6b0e
Daniel Santos <danielsantos7@outlook.com.br>
2026-08-04 12:21:29 -0300

    Remove the six tests that cannot pass the Spike diff

--- verif/tests/halt_direct/main.s
+++
@@ -1,7 +0,0 @@
-.include "defs.inc"
-
-.globl _start
-_start:
-    li x1, HALT_CMD_DATA
-    li x2, HALT_CMD_ADDR
-    sw x1, 0(x2)
--- verif/tests/halt_only/main.s
+++
@@ -1,5 +0,0 @@
-.include "defs.inc"
-
-.globl _start
-_start:
-    call finish_test
